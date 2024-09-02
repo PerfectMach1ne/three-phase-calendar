5bcd370490d748804b77e60556291121463c2919
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2024-09-03 00:09:18 +0200

    add dep: Gson; chore

--- api-server/app/build.gradle.kts
+++ api-server/app/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+    implementation("com.google.code.gson:gson:2.11.0")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
