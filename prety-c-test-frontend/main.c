c756bd12a1fc2db3b5936487b1ad840fcf28820a
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2024-09-05 16:58:27 +0200

    begin ncurses rewrite

--- prety-c-test-frontend/main.c
+++ prety-c-test-frontend/main.c
@@ -7,6 +7,137 @@
 #include <curl/curl.h>
 #include <ncurses.h>
 
+#define SMALL_Q 113
+
+WINDOW* create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW* local_win);
+// delete this later and replace with proper functions
+void draw_topleftbox(); void draw_datestring(); void draw_monthdays(); void draw_weekdays();
+
+int main(int argc, char** argv) {
+  WINDOW* test_box;
+  int height, width, starty, startx;
+  int ch;
+
+  CURL* curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+  
+  initscr();
+  cbreak(); // Disable line buffering
+  keypad(stdscr, TRUE);
+  noecho();
+
+  height = 5; width = 7;
+  starty = 1; startx = 1;
+  test_box = create_newwin(height, width, starty, startx);
+  wborder(test_box, '|', '|', '-', '-', '+', '+', '+', '+');
+
+  while ( (ch = getch()) != SMALL_Q ) {
+    wrefresh(test_box);
+    switch (ch) {
+      case 'h':
+      case 'H':
+        destroy_win(test_box);  
+        test_box = create_newwin(height, width, starty, --startx);
+        wborder(test_box, '|', '|', '-', '-', '+', '+', '+', '+');
+        break;
+      case 'j':
+      case 'J':
+        destroy_win(test_box);  
+        test_box = create_newwin(height, width, --starty, startx);
+        wborder(test_box, '|', '|', '-', '-', '+', '+', '+', '+');
+        break;
+      case 'k':
+      case 'K':
+        destroy_win(test_box);  
+        test_box = create_newwin(height, width, ++starty, startx);
+        wborder(test_box, '|', '|', '-', '-', '+', '+', '+', '+');
+        break;
+      case 'l':
+      case 'L':
+        destroy_win(test_box);  
+        test_box = create_newwin(height, width, starty, ++startx);
+        wborder(test_box, '|', '|', '-', '-', '+', '+', '+', '+');
+        break;
+    }
+    wrefresh(test_box);
+    // if (ch == CTRL_Q) break;
+    // draw_topleftbox();
+    // draw_datestring();
+
+    // draw_monthdays();
+    // draw_weekdays();
+  }
+  
+  // if (curl) {
+  //   curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:8057/testTask");
+  //   /* Do not do the transfer - only connect to host */
+  //   // curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 1L);
+
+  //   res = curl_easy_perform(curl);
+
+  //   if (res != CURLE_OK) {
+  //     fprintf(stderr, "curl_easy_perform() failed: %s\n",
+  //                   curl_easy_strerror(res));
+  //   } else {
+  //     char buf[512];
+  //     size_t nread;
+  //     long sockfd;
+  //     char *ct;
+  //     res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
+      
+  //     // this doesn't work lol
+  //     if ((CURLE_OK == res) && ct) {
+  //       printf("Wondrous! We have received Content-Type %s!\n", &ct);
+  //     }
+
+  //     res = curl_easy_getinfo(curl, CURLINFO_ACTIVESOCKET, &sockfd);
+
+  //     res = curl_easy_recv(curl, buf, sizeof(buf), &nread);
+  //     printf("%s", &buf);
+  //   }
+
+  //   curl_easy_cleanup(curl);
+  // }
+  refresh();
+
+  endwin();
+  return EXIT_SUCCESS;
+}
+
+WINDOW* create_newwin(int height, int width, int starty, int startx) {
+  WINDOW* local_win;
+  
+  local_win = newwin(height, width, starty, startx);
+  box(local_win, 0, 0);
+  // wborder(local_win, '|', '|', '-', '-', '+', '+', '+', '+');
+  wrefresh(local_win); // Refresh the window to show the bocx
+
+  return local_win;
+}
+
+void destroy_win(WINDOW* local_win) {
+  wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+  wrefresh(local_win);
+  delwin(local_win);
+}
+
+void draw_topleftbox() {
+  printf("╔");
+  for (int i = 0; i < 5; i++) {
+    printf("═");
+  }
+  // printf("╠");
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; i < 5; i++) {
+      printf(" ");
+    }
+    // printf("\n");
+  }
+}
+
 void draw_datestring() {
   time_t t = time(NULL);
   struct tm* lt = localtime(&t);
@@ -14,7 +145,7 @@ void draw_datestring() {
 
   // Draw 1st box ceiling
   for (int i = 0; i < 43; i++) {
-    if (i == 0) printf("╔");
+    if (i == 0) printf("╦");
     else if (i < 42 ) printf("═");
     else printf("╗\n");
   }
@@ -107,77 +238,34 @@ void draw_weekdays() {
   // Draw 2nd box floor & 3rd box ceiling
   for (int i = 0; i < sizeof(weekdays) / sizeof(char*); i++) {
     if (i == 0) {
-      printf("╠═════╬"); 
+      printw("╠═════╬"); 
     } else if (i == 6) {
-      printf("═════╣\n"); 
+      printw("═════╣\n"); 
     } else {
-      printf("═════╬"); 
+      printw("═════╬"); 
     }
   }
 
   // Draw 3rd box content & walls
   for (int j = 0; j < sizeof(weekdays) / sizeof(char*); j++) { 
     if (j == 0) {
-      printf("║ %s ║", weekdays[j]); 
+      printw("║ %s ║", weekdays[j]); 
     } else if (j == 6) {
-      printf(" %s ║\n", weekdays[j]); 
+      printw(" %s ║\n", weekdays[j]); 
     } else {
-      printf(" %s ║", weekdays[j]); 
+      printw(" %s ║", weekdays[j]); 
     }
   }
 
   // Draw 3rd box floor
   for (int k = 0; k < sizeof(weekdays) / sizeof(char*); k++) { 
     if (k == 0) {
-      printf("╚═════╩"); 
+      printw("╚═════╩"); 
     } else if (k == 6) {
-      printf("═════╝\n"); 
+      printw("═════╝\n"); 
     } else {
-      printf("═════╩"); 
+      printw("═════╩"); 
     }
   }
-}
-
-int main(int argc, char** argv) {
-  CURL* curl;
-  CURLcode res;
-
-  curl = curl_easy_init();
-
-  draw_datestring();
-  draw_monthdays();
-  draw_weekdays();
-  
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:8057/testTask");
-    /* Do not do the transfer - only connect to host */
-    // curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 1L);
-
-    res = curl_easy_perform(curl);
-
-    if (res != CURLE_OK) {
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-    } else {
-      char buf[512];
-      size_t nread;
-      long sockfd;
-      char *ct;
-      res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
-      
-      // this doesn't work lol
-      if ((CURLE_OK == res) && ct) {
-        printf("Wondrous! We have received Content-Type %s!\n", &ct);
-      }
-
-      res = curl_easy_getinfo(curl, CURLINFO_ACTIVESOCKET, &sockfd);
-
-      res = curl_easy_recv(curl, buf, sizeof(buf), &nread);
-      printf("%s", &buf);
-    }
-
-    curl_easy_cleanup(curl);
-  }
-
-  return EXIT_SUCCESS;
+  // refresh();
 }
