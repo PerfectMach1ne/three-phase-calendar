ecc8b86bbc6dc3894308e65f633cb1df7dafede3
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2025-06-06 11:36:51 +0200

    fix freshly broken Composition API port
    Signed-off-by: starr4ever@TransEdenV4 <starflowerr777@gmail.com>

--- tauri-app/src/components/buttons/ChangeWeek.vue
+++ tauri-app/src/components/buttons/ChangeWeek.vue
@@ -1,34 +1,34 @@
 <script setup>
-import { created, ref } from 'vue';
+import { ref } from 'vue';
 
-defineProps({
-  character: String
-});
-defineEmits([
-  'past-week', 'future-week'
-]);
+const props = defineProps(['character']);
+const emits = defineEmits(['past-week', 'future-week']);
 
 const displayChar = ref("");
 const buttonType = ref("");
 
-created(() => {
-  displayChar.value = character.value;
-  
-  if ( Number(displayChar.value.slice(2,7)) == 10094 ) buttonType.value = 'left';
-  else if ( Number(displayChar.value.slice(2,7)) == 10095 ) buttonType.value = 'right';
-});
+// onBeforeMount(() => {
+console.log(props.character);
+console.log(displayChar.value);
+displayChar.value = props.character;
+console.log(props.character);
+console.log(displayChar.value);
+
+if ( Number(displayChar.value.slice(2,7)) == 10094 ) buttonType.value = 'left';
+else if ( Number(displayChar.value.slice(2,7)) == 10095 ) buttonType.value = 'right';
+// });
 </script>
 
 <template>
   <button
-    v-if="buttonType == 'left'"
+    v-if="buttonType === 'left'"
     class="change__week"
-    v-html="this.displayChar"
+    v-html="displayChar"
     @click="$emit('past-week')"></button>
   <button
-    v-else-if="buttonType == 'right'"
+    v-else-if="buttonType === 'right'"
     class="change__week"
-    v-html="this.displayChar"
+    v-html="displayChar"
     @click="$emit('future-week')"></button>
   <p v-else>An idiotic error occurred while trying to display the button.</p> <!-- lol -->
 </template>
--- tauri-app/src/components/buttons/TodaysWeek.vue
+++ tauri-app/src/components/buttons/TodaysWeek.vue
@@ -1,24 +1,20 @@
 <script setup>
-import { created, ref } from 'vue';
+import { ref } from 'vue';
 
-defineProps({
-  character: String
-});
-defineEmits([
-  'todays-week'
-]);
+const props = defineProps(['character']);
+const emits = defineEmits(['todays-week']);
 
 const displayChar = ref("");
 
-created(() => {
-  displayChar.value = character.value;
-});
+// onBeforeMount(() => {
+displayChar.value = props.character;
+// });
 </script>
 
 <template>
   <button
     class="change__week"
-    v-html="this.displayChar"
+    v-html="displayChar"
     @click="$emit('todays-week')"
   ></button>
 </template>
