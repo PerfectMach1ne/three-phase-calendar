ba4952a2ce9ba08f002a64866e9a6f945a88bfd8
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2025-06-19 17:23:03 +0200

    test Vue-Quill for journal mode
    Signed-off-by: starr4ever@TransEdenV4 <starflowerr777@gmail.com>
    signed:

--- tauri-app/src/components/containers/JournalContainer.vue
+++ tauri-app/src/components/containers/JournalContainer.vue
@@ -31,20 +31,6 @@ export default {
       a GitHub repo link and all the technologies listed nicely (just because it'd look cool).
     </h1>
     <p>AboutPage</p>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
-    <p>loooong looong teeeext to test things</p><br>
   </div>
 </template>
 
